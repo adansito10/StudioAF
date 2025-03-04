f7db5a71fad9ab0af9feb8d453126be48cedf597
adansito10 <adandejesus200420@gmail.com>
2025-03-03 22:25:55 -0600

    agregar nuevos diseños para login y registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,60 +4,63 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    redirectTo: 'login', // Redirige a login al abrir la app
+    pathMatch: 'full'
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
   },
   {
     path: 'registro',
-    loadChildren: () => import('./pages/registro/registro.module').then( m => m.RegistroPageModule)
+    loadChildren: () => import('./pages/registro/registro.module').then(m => m.RegistroPageModule)
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
     path: 'detalle-servicio',
-    loadChildren: () => import('./pages/detalle-servicio/detalle-servicio.module').then( m => m.DetalleServicioPageModule)
+    loadChildren: () => import('./pages/detalle-servicio/detalle-servicio.module').then(m => m.DetalleServicioPageModule)
   },
   {
     path: 'pago',
-    loadChildren: () => import('./pages/pago/pago.module').then( m => m.PagoPageModule)
+    loadChildren: () => import('./pages/pago/pago.module').then(m => m.PagoPageModule)
   },
   {
     path: 'formulario',
-    loadChildren: () => import('./pages/formulario/formulario.module').then( m => m.FormularioPageModule)
+    loadChildren: () => import('./pages/formulario/formulario.module').then(m => m.FormularioPageModule)
   },
   {
     path: 'gallery',
-    loadChildren: () => import('./pages/gallery/gallery.module').then( m => m.GalleryPageModule)
+    loadChildren: () => import('./pages/gallery/gallery.module').then(m => m.GalleryPageModule)
   },
   {
     path: 'servicio-domicilio',
-    loadChildren: () => import('./pages/servicio-domicilio/servicio-domicilio.module').then( m => m.ServicioDomicilioPageModule)
+    loadChildren: () => import('./pages/servicio-domicilio/servicio-domicilio.module').then(m => m.ServicioDomicilioPageModule)
   },
-{
-  path: 'metodo-pago',
-  loadChildren: () => import('./pages/metodo-pago/metodo-pago.module').then( m => m.MetodoPagoPageModule)
-},
+  {
+    path: 'metodo-pago',
+    loadChildren: () => import('./pages/metodo-pago/metodo-pago.module').then(m => m.MetodoPagoPageModule)
+  },
   {
     path: 'pago-confirmado',
-    loadChildren: () => import('./pago-confirmado/pago-confirmado.module').then( m => m.PagoConfirmadoPageModule)
+    loadChildren: () => import('./pago-confirmado/pago-confirmado.module').then(m => m.PagoConfirmadoPageModule)
   },
   {
     path: 'consultas',
-    loadChildren: () => import('./consultas/consultas.module').then( m => m.ConsultasPageModule)
+    loadChildren: () => import('./consultas/consultas.module').then(m => m.ConsultasPageModule)
   },
   {
     path: 'nueva-pagina',
-    loadChildren: () => import('./nueva-pagina/nueva-pagina.module').then( m => m.NuevaPaginaPageModule)
+    loadChildren: () => import('./nueva-pagina/nueva-pagina.module').then(m => m.NuevaPaginaPageModule)
   },
   {
     path: 'resumen-servicio',
-    loadChildren: () => import('./resumen-servicio/resumen-servicio.module').then( m => m.ResumenServicioPageModule)
+    loadChildren: () => import('./resumen-servicio/resumen-servicio.module').then(m => m.ResumenServicioPageModule)
   }
-  
- 
-  
 ];
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
